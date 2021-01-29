e3389589c8654528d6b7d4c24b7441ef61b12c08
Jair Valadez <val20013@byui.edu>
2021-01-28 21:46:45 -0600

    Updated HelloFromKarly.java

--- src/com/company/HelloFromKarly.java
+++ src/com/company/HelloFromKarly.java
@@ -7,6 +7,8 @@ public class HelloFromKarly {
 	System.out.println("I hate when I mess up");
 	System.out.println("This is annoying");
 	System.out.println("But now I'm ready to commit");
-	System.out.println("I'm changing it up down here.");    
+	System.out.println("I'm changing it up down here.");
+	System.out.println("Hello Karly, my name is Jair Valadez");
+	System.out.println("And don't worry everything is good");
     }
 }
