79187f5f56b34d82f23801ea754b2408fe9b0bc3
Karly Gay <gay17005@byui.edu>
2021-01-28 19:08:48 -0700

    This is it! PUSH!

--- src/com/company/HelloFromKarly.java
+++ src/com/company/HelloFromKarly.java
@@ -6,5 +6,6 @@ public class HelloFromKarly {
 	System.out.println("Hello, my name is Karly");
 	System.out.println("I hate when I mess up");
 	System.out.println("This is annoying");
+	System.out.println("But now I'm ready to commit");
     }
 }
